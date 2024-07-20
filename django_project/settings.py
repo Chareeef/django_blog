026a9869569eaa54916dca83033bb13481f6570e
Chareeef <youssef.charif.h@gmail.com>
2024-07-20 21:01:26 +0100

    Put conditional on DEBUG setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['youssef-django-blog-392795c250e4.herokuapp.com']
 
